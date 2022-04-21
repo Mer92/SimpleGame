83266271efde04f0afbcb5c2eca2553734616907
mer92 <remeil@hotmail.co.uk>
2022-04-21 19:50:11 +0100

    Added Js folder and index doc

---
+++ Js/script.js
@@ -0,0 +1,37 @@
+// A computerPlay function is created
+function computerPlay(){
+
+}
+// The computer randomly outputs either rock, paper or scissors
+// A single round function is created
+function round(playerSelection, computerSelection){
+
+}
+// The function takes the parameters of playerSelection and computerSelection
+// When the round is over a message is displayed stating a winner or loser of the round
+// The playerSelection parameter is case insensitive
+// A playRound function is declared
+function playRound(playerSelection, computerSelection) { 
+    
+    // your code here! 
+
+} 
+
+/* const playerSelection = "rock"; 
+const computerSelection = computerPlay(); 
+console.log(playRound(playerSelection, computerSelection)); */
+// The results are returned in the playRound function
+// A game function is created 
+function game(){
+    playRound();
+}
+// The playRound function is called inside the game function
+
+// A 5 round game keeps score, reports the winner or loser at the end of each round game
+// Loop from 0 to 5 
+for (let i = 0; i < 5; i++) {
+    // your code here!
+ }
+
+// The results and winner of each round at the end  are seen in the console 
+// The user uses a prompt method to input selection, either rock, paper or scissors 
